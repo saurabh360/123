1ca408a8c7f8c09e54e2cac6c0ccd071bd7c8957
unknown-user <unknown-user@10.211.55.2>
2013-12-31 18:08:43 +0530

    Initial check-in of module ewdfe34e

---
+++ ewdfe34eViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace ewdfe34e
+{
+	public partial class ewdfe34eViewController : UIViewController
+	{
+		public ewdfe34eViewController () : base ("ewdfe34eViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
